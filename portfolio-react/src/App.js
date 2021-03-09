54fa09334ea01c3795cefca6e12ac6bfa827a557
Laura Gwen <lauraburch.lb@gmail.com>
2021-03-09 16:20:35 -0500

    added a navBar and links

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,48 +1,72 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, NavLink } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './App.css';
+
 
 class App extends React.Component {
-  
-  constructor(props){
-  super(props);
-  this.state = {  
 
-    title: "Laura Gwendolyn Burch",
-    headerLinks:[
-      { title: "Home", paths: "/"},
-      { title: "About", paths: "/about"},
-      { title: "Contact", paths: "/contact"},
-      { title: "Resume", paths: "/resume"},
-      { title: "Portfolio", paths: "/portfolio"}
-    ],
-    home: {
-      title: "Full Stack Website Developer",
-      subtitle: "Ideas | Creating | Design",
-      text: "Portfolio"
-    },
-    about: {
-      title: "About Me",
-    },
-    contact: {
-      title: "Choose your own adventure",
-    },
-    resume: {
-      title: "Resume",
-    },
-    portfolio: {
-      title: "Portfolio",
-    },
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      title: "Laura Gwendolyn Burch",
+      headerLinks: [
+        { title: "Home", paths: "/" },
+        { title: "About", paths: "/about" },
+        { title: "Contact", paths: "/contact" },
+        { title: "Resume", paths: "/resume" },
+        { title: "Portfolio", paths: "/portfolio" }
+      ],
+      home: {
+        title: "Full Stack Website Developer",
+        subtitle: "Ideas | Creating | Design",
+        text: "Portfolio"
+      },
+      about: {
+        title: "About Me",
+      },
+      contact: {
+        title: "Choose your own adventure",
+      },
+      resume: {
+        title: "Resume",
+      },
+      portfolio: {
+        title: "Portfolio",
+      },
 
 
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <Container className="p-o" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Laura Gwendolyn Burch</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+            <Switch> 
+            <Nav className="ml-auto">
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+              <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
+              <Nav.Link as={NavLink} to="/resume">Resume</Nav.Link>
+              <Nav.Link as={NavLink} to="/aboutMe">About Me</Nav.Link>
+              <Nav.Link as={NavLink} to="/portfolio">Portfolio</Nav.Link>
+              </Nav>
+              </Switch> 
+            </Navbar.Collapse>
+            </Navbar>
+        </Container>
+      </Router>
+    );
   }
 }
 
-  render() { 
-    return ( 
-      <div>hello from react</div>
-     );
-  }
-}
- 
 export default App;
 
 
