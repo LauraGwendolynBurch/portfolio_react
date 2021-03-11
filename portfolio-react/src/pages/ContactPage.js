e7c055aa57376a4eb1bdeef9af8fde0b2df24f41
Laura Gwen <lauraburch.lb@gmail.com>
2021-03-10 22:29:53 -0500

    added submit forms and styles for messages

--- portfolio-react/src/pages/ContactPage.js
+++ portfolio-react/src/pages/ContactPage.js
@@ -20,12 +20,31 @@ class ContactPage extends React.Component {
         }
     }
 
+    handleChange = (event) => {
+       const target = event.target;
+        const value = target.type === `checkbox` ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState ({
+            [name]: value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        
+        this.setState ({
+            disabled: true
+        })
+
+    }
+
     render() {
         return (
             <div>
                 <Hero title={this.props.title} />
                 <Content>
-                    <Form>
+                    <Form onSubmit={this.handleSubmit}>
                         <Form.Group>
                             <Form.Label htmlFor="full-name">Full Name</Form.Label>
                             <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange}/>
@@ -44,6 +63,9 @@ class ContactPage extends React.Component {
                         <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled} >
                         Send
                         </Button>
+
+                        {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
+                        {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
                     </Form>
                 </Content>
             </div>
